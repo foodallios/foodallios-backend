564f655f71bee6f3c9bf8c97f5ba9715530eccdf
aristotelissym <foodallios@gmail.com>
2022-12-01 23:50:20 +0200

    dto for creation of owner and upd order

---
+++ src/dto/shop_owner/createShopOwnerDto.ts
@@ -0,0 +1,8 @@
+import { Users } from "src/models/users.model"
+
+export class createShopOwnerDto {
+
+    user: string;
+    createdBy: string;
+    createdAt: Date;
+}
--- src/dto/table_orders/editOrderDto.ts
+++ src/dto/table_orders/editOrderDto.ts
@@ -1,4 +1,4 @@
 export class editOrderDto {
-    
+    id: string;
     quantity: number;
 }
