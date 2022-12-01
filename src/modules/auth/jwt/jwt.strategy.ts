08c7b03de65fff7d8a850dc6c4e77ede647202a6
aristotelissym <foodallios@gmail.com>
2022-12-01 23:51:25 +0200

    payload with username password

--- src/modules/auth/jwt/jwt.strategy.ts
+++ src/modules/auth/jwt/jwt.strategy.ts
@@ -8,13 +8,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: true,
       secretOrKey: jwtConstants.secret,
     });
   }
 
   async validate(payload: any) {
     console.log(payload)
-    return { userId: payload.sub, username: payload.username };
+    return { username: payload.username, password: payload.password };
   }
 }
